64e4b39597f2d49f8dda3912959ccbae2383b157
Zain Shk <zshk599@gmail.com>
2026-04-29 02:19:35 +0500

    update align

--- src/components/containers/OurMission.tsx
+++ src/components/containers/OurMission.tsx
@@ -5,32 +5,26 @@ const OurMission = () => {
   return (
     <section className="section mission-s fade-wrapper">
       <div className="container">
-        <div className="row">
-          <div className="col-12">
-            <div className="section__header--secondary">
-              <div className="row gaper align-items-center">
-                <div className="col-12 col-lg-5 col-xxl-5">
-                  <div className="section__header text-center text-lg-start mb-0">
-                    <span className="sub-title">
-                      mission & vission
-                      <i className="fa-solid fa-arrow-right"></i>
-                    </span>
-                    <h2 className="title title-anim">Built On Systems. Driven By Results.
-</h2>
-                  </div>
-                </div>
-                <div className="col-12 col-lg-7 col-xxl-5 offset-xxl-2">
-                  <div className="text-center text-lg-start">
-                    <p>
-                     We don’t just build software. We design AI, SaaS, and CRM systems that solve real business problems, remove bottlenecks, and create predictable growth. Every decision we make is focused on performance, clarity, and long-term scalability.
-
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <div className="row gaper align-items-start" style ={{paddingBottom:'20px'}}>
+  <div className="col-12 col-lg-5 col-xxl-5">
+    <div className="section__header text-center text-lg-start mb-0">
+      <span className="sub-title">
+        mission & vission
+        <i className="fa-solid fa-arrow-right"></i>
+      </span>
+      <h2 className="title title-anim">Built On Systems. Driven By Results.</h2>
+    </div>
+  </div>
+  <div className="col-12 col-lg-7 col-xxl-5 offset-xxl-2">
+    <div className="text-center text-lg-start" style={{ paddingTop: "52px" }}>
+      <p>
+        We don't just build software. We design AI, SaaS, and CRM systems that solve real business
+        problems, remove bottlenecks, and create predictable growth. Every decision we make is
+        focused on performance, clarity, and long-term scalability.
+      </p>
+    </div>
+  </div>
+</div>
         <div className="row gaper">
           <div className="col-12 col-lg-6">
             <div className="mission-s__single mission-s__single--alt fade-top">
